6aba0aac9233af2abba6ae18c6407541219dd377
HGIANG <dhgiang85@gmail.com>
2018-05-03 08:55:57 +0700

    Add project file

---
+++ TeducoreAppE/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TeducoreAppE.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
